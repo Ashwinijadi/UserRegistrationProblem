f09fc73b5de62369bfc2f1042782095cccd5ec2e
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-01 22:58:22 +0530

    added:In User Registration System Email validification is added

--- Registration.java
+++ Registration.java
@@ -6,19 +6,18 @@ import java.util.regex.Pattern;
 public class Registration {
 public static void main(String[] args) {
 	Scanner scanner=new Scanner(System.in);
-    System.out.println("Enter Last Name");
+    System.out.println("Enter your Email");
     String name=scanner.nextLine();
     System.out.println(name);
-    //String pattern="^(91)\\s{1}[7-9][0-9]{9}$";  
-    String lastName="^[A-Z]{1}[a-z]{2,}$" ;
-    Pattern namePattern1=Pattern.compile(lastName);
-    Matcher m1=namePattern1.matcher(name);
+    String emailPattern="[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}";
+    Pattern Pattern2=Pattern.compile(emailPattern);
+    Matcher m1= Pattern2.matcher(name);
     System.out.println("Match - "+m1.matches());
     if(m1.matches()==true)
     {
- 	   System.out.println("your Last Name is valid");
+ 	   System.out.println("your Email is valid");
     }
     else {
- 	   System.out.println("your Last Name is not valid");}
+ 	   System.out.println("your Email is not valid");}
 }
 }
