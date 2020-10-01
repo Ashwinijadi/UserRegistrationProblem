084360e838031e1283d27e9949bea2e904e66b5a
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-01 18:30:06 +0530

    added:Welcome to UserRegistration Program

---
+++ Registration.java
@@ -0,0 +1,8 @@
+package com.userRegistration;
+
+public class Registration {
+public static void main(String[] args) {
+       
+    	   System.out.println("Welcome to UserRegistration Program");}
+}
+
