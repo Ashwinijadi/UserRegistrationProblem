547eeab41ff456095e36a97a8c8a8e45de4f1567
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-01 20:46:00 +0530

    added:In User Registration System first name is added

--- Registration.java
+++ Registration.java
@@ -1,8 +1,24 @@
 package com.userRegistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Registration {
 public static void main(String[] args) {
-       
-    	   System.out.println("Welcome to UserRegistration Program");}
+	Scanner scanner=new Scanner(System.in);
+    System.out.println("Enter FirstName");
+    String name=scanner.nextLine();
+    System.out.println(name);
+    //String pattern="^(91)\\s{1}[7-9][0-9]{9}$";  
+    String firstName="^[A-Z]{1}[a-z]{2,}$" ;
+    Pattern namePattern=Pattern.compile(firstName);
+    Matcher m=namePattern.matcher(name);
+    System.out.println("Match - "+m.matches());
+    if(m.matches()==true)
+    {
+ 	   System.out.println("your First Name is valid");
+    }
+    else {
+ 	   System.out.println("your First Name is not valid");}
 }
-
+}
