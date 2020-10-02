af25ff4cad2007ef2ef66242cd5bd4c449e78d47
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-02 11:59:38 +0530

    added:In User Registration System Atleast One Number Verification in Password is added

--- Registration.java
+++ Registration.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
     System.out.println("Enter your Username");
     String name=scanner.nextLine();
     System.out.println(name);
-    String usernamePattern="^(?=.*[A-Z])[1-9A-Za-z/@/#]{8,}$";
+    String usernamePattern="^(?=.*[1-9])[1-9A-Za-z/@/#]{8,}$";
     Pattern Pattern3=Pattern.compile(usernamePattern);
     Matcher m2= Pattern3.matcher(name);
     System.out.println("Match - "+m2.matches());
